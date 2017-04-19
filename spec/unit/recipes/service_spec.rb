87b37d4b3d834450249f9c10d4e1c0501acb23a0
DC Barringer <nukdcbear@gmail.com>
2017-04-19 13:28:32 -0400

    Defined rspec unit tests

--- spec/unit/recipes/service_spec.rb
+++ spec/unit/recipes/service_spec.rb
@@ -11,12 +11,21 @@ describe 'httpd::service' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.3.1611')
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
+
+    it 'starts the necessary service' do
+      expect(chef_run).to start_service('httpd')
+    end
+
+    it 'enables the necessary service' do
+      expect(chef_run).to enable_service('httpd')
+    end
+    
   end
 end
